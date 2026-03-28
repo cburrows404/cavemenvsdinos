8ca8f0bb0951e81b512eaf582b5d93b1108c05fb
ahorne6 <elaine.horne82@gmail.com>
2026-03-28 14:00:22 -0400

    Squashed commit of the following:
    commit 8244f85d7520f449379c79b7521eb8db8f5dd910
    Author: ahorne6 <elaine.horne82@gmail.com>
    Date:   Sat Mar 28 13:59:28 2026 -0400
    
        Caveman setup

---
+++ p5/scripts/caveman.js
@@ -0,0 +1,21 @@
+
+class caveman{
+    constructor(posX, posY, speed, range, strength, id){
+        this.posX =posX;
+        this.posY =posY;
+        this.speed =speed;
+        this.range =range;
+        this.strength = strength;
+        this.id = id;
+    };
+    calculateClosestDino(dinoList){
+        //for each existing dino, calculate if dist(self, dino) < range and save smallest number
+        //return dino within range with least distance
+        
+    }
+    attack(closestDinoID){
+        //play attack animation and deal strength damage to selected dino
+    }
+
+
+}
